5681868d2b3ac9798a633a2600b4829146c01256
AUTOBOTS <85267187+AUTOBOTS00@users.noreply.github.com>
2021-08-18 06:11:49 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,92 +23,26 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram import Client, filters
 from utils import USERNAME, mp
 from config import Config
-U=USERNAME
-CHAT=Config.CHAT
-msg=Config.msg
-HOME_TEXT = "<b>Helo, [{}](tg://user?id={})\n\nIam MusicPlayer 2.0 which plays music in Channels and Groups 24*7.\n\nI can even Stream Youtube Live in Your Voicechat.\n\nDeploy Your Own bot from source code below.\n\nHit /help to know about available commands.</b>"
-HELP = """
 
-<b>Add the bot and User account in your Group with admin rights.
+ABS="Developer"
+APPER="shamilhabeeb"
+OWNER="Owner"
+GITCLONE="github.com/shamilhabeebnelli/song-bot"
+B2="telegram.dog/shamilnelli"
+BUTTON1="📜 Source Code 📜"
 
-Start a VoiceChat.
-
-Use /play <song name> or use /play as a reply to an audio file or youtube link.
-
-You can also use /splay <song name> to play a song from Jio Saavn or /cplay <channel username or channel id> to play music from a telegram channel.</b>
-
-**Common Commands**:
-
-**/play**  Reply to an audio file or YouTube link to play it or use /play <song name>.
-**/splay** Play music from Jio Saavn, Use /splay <song name>
-**/player**  Show current playing song.
-**/help** Show help for commands
-**/playlist** Shows the playlist.
-
-**Admin Commands**:
-**/skip** [n] ...  Skip current or n where n >= 2
-**/join**  Join voice chat.
-**/leave**  Leave current voice chat
-**/shuffle** Shuffle Playlist.
-**/cplay** Play music from a channel's music files.
-**/vc**  Check which VC is joined.
-**/stop**  Stop playing.
-**/radio** Start Radio.
-**/stopradio** Stops Radio Stream.
-**/clearplaylist** Clear the playlist.
-**/replay**  Play from the beginning.
-**/clean** Remove unused RAW PCM files.
-**/pause** Pause playing.
-**/resume** Resume playing.
-**/volume** Change volume(0-200).
-**/mute**  Mute in VC.
-**/unmute**  Unmute in VC.
-**/restart**  Update and restarts the Bot.
-"""
-
-
-
-
-@Client.on_message(filters.command(['start', f'start@{U}']))
+@Client.on_message(filters.command('start') & filters.private)
 async def start(client, message):
-    buttons = [
-        [
-        InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),
-        InlineKeyboardButton('🤖 Other Bots', url='https://t.me/subin_works/122'),
-    ],
-    [
-        InlineKeyboardButton('👨🏼‍💻 Developer', url='https://t.me/subinps'),
-        InlineKeyboardButton('🧩 Source', url='https://github.com/subinps/MusicPlayer'),
-    ],
-    [
-        InlineKeyboardButton('👨🏼‍🦯 Help', callback_data='help'),
-        
-    ]
-    ]
-    reply_markup = InlineKeyboardMarkup(buttons)
-    m=await message.reply(HOME_TEXT.format(message.from_user.first_name, message.from_user.id), reply_markup=reply_markup)
-    await mp.delete(m)
-    await mp.delete(message)
-
-
-
-@Client.on_message(filters.command(["help", f"help@{U}"]))
-async def show_help(client, message):
-    buttons = [
-        [
-            InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),
-            InlineKeyboardButton('🤖 Other Bots', url='https://t.me/subin_works/122'),
-        ],
-        [
-            InlineKeyboardButton('👨🏼‍💻 Developer', url='https://t.me/subinps'),
-            InlineKeyboardButton('🧩 Source', url='https://github.com/subinps/MusicPlayer'),
-        ]
-    ]
-    reply_markup = InlineKeyboardMarkup(buttons)
-    if msg.get('help') is not None:
-        await msg['help'].delete()
-    msg['help'] = await message.reply_text(
-        HELP,
-        reply_markup=reply_markup
-        )
-    await mp.delete(message)
+    await message.reply_photo(photo=Config.START_IMG, caption=Config.START_MSG.format(message.from_user.mention),
+         reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(BUTTON1, url=GITCLONE)
+                 ],[
+                    InlineKeyboardButton(OWNER, url=f"https://telegram.dog/shamilhabeeb"),
+                    InlineKeyboardButton(ABS, url=B2)
+            ]
+          ]
+        ),
+        reply_to_message_id=message.message_id
+    )
