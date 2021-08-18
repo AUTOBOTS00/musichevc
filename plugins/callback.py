e3f753c64dda3d0b73b8a47f334114dfb1a496a0
AUTOBOTS <85267187+AUTOBOTS00@users.noreply.github.com>
2021-08-18 06:15:08 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -26,46 +26,6 @@ from utils import mp, playlist
 from config import Config
 
 
-HELP = """
-
-<b>Add the bot and User account in your Group with admin rights.
-
-Start a VoiceChat
-
-Use /play <song name> or use /play as a reply to an audio file or youtube link.
-
-You can also use /splay <song name> to play a song from JioSaavn or /cplay <channel username or channel id> to play music from a telegram channel.</b>
-
-**Common Commands**:
-
-**/play**  Reply to an audio file or YouTube link to play it or use /play <song name>.
-**/splay** Play music from Jio Saavn, Use /splay <song name>
-**/player**  Show current playing song.
-**/help** Show help for commands
-**/playlist** Shows the playlist.
-
-**Admin Commands**:
-**/skip** [n] ...  Skip current or n where n >= 2
-**/join**  Join voice chat.
-**/leave**  Leave current voice chat
-**/shuffle** Shuffle Playlist.
-**/cplay** Play music from a channel's music files.
-**/vc**  Check which VC is joined.
-**/stop**  Stop playing.
-**/radio** Start Radio.
-**/stopradio** Stops Radio Stream.
-**/clearplaylist** Clear the playlist.
-**/replay**  Play from the beginning.
-**/clean** Remove unused RAW PCM files.
-**/pause** Pause playing.
-**/resume** Resume playing.
-**/volume** Change volume(0-200).
-**/mute**  Mute in VC.
-**/unmute**  Unmute in VC.
-**/restart** Update and restarts the Bot.
-"""
-
-
 
 @Client.on_callback_query()
 async def cb_handler(client: Client, query: CallbackQuery):
@@ -207,23 +167,4 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 ]
             )
         )
-        except:
-            pass
-    elif query.data=="help":
-        buttons = [
-            [
-                InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),
-                InlineKeyboardButton('🤖 Other Bots', url='https://t.me/subin_works/122'),
-            ],
-            [
-                InlineKeyboardButton('👨🏼‍💻 Developer', url='https://t.me/subinps'),
-                InlineKeyboardButton('🧩 Source', url='https://github.com/subinps/MusicPlayer'),
-            ]
-            ]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await query.edit_message_text(
-            HELP,
-            reply_markup=reply_markup
-
-        )
-
+        
